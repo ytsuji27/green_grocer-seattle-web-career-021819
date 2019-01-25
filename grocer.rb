bb5143dc30c5ec092b398c3a6207293d04100b87
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-25 06:28:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,25 +15,18 @@ require `pry`
 #end
 
 
-def apply_coupons
-  
-  cart = {"AVOCADO" => {:price => 3.0, :clearance => true, :count => 2}}
-  coupons = {:item => "AVOCADO", :num => 2, :cost => 5.0}
-  
-  coupons_applied = {}
+def apply_coupons(cart, coupons)
+  coupons_applied_cart = cart
   coupons.each do |coupon|
     name = coupon[:item]
-    binding.pry
     if name == cart[name] && cart[name][:count] >= coupon[:num]
-      coupons_applied["#{name} W/ COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => (cart[name][:count]/coupon[:num])}
-      coupons_applied[name] = cart[name]
-      coupons_applied[name][:count] = (cart[name][:count]%coupon[:num])
-    else
-      coupons_applied[name] = cart[name]
+      coupons_applied_cart["#{name} W/ COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => (cart[name][:count]/coupon[:num])}
+      coupons_applied_cart[name][:count] = (cart[name][:count]%coupon[:num])
     end
   end
+  coupons_applied_cart
 end
-apply_coupons
+
 
 #def apply_clearance(cart)
   # code here
